c86aafd75c150d13d4e477e0e5a34d3be0e6ca5e
Kirill Uksusov <uksusoff@yandex.ru>
2024-12-03 14:41:54 +0400

    webtrees 2.2 support added

--- docker/_modules.sh
+++ docker/_modules.sh
@@ -9,3 +9,6 @@ ENV_FILE="${SCRIPT_DIR}/.env"
 source "${ENV_FILE}"
 
 rm -rf "${MODULES_DIR}/faces" && git clone https://github.com/UksusoFF/webtrees-faces "${MODULES_DIR}/faces"
+rm -rf "${MODULES_DIR}/reminder" && git clone https://github.com/UksusoFF/webtrees-reminder "${MODULES_DIR}/reminder"
+rm -rf "${MODULES_DIR}/mdi" && git clone https://github.com/UksusoFF/webtrees-mdi "${MODULES_DIR}/mdi"
+rm -rf "${MODULES_DIR}/photos" && git clone https://github.com/UksusoFF/webtrees-photos "${MODULES_DIR}/photos"
